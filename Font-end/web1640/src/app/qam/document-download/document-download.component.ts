bd93dc103cb639cdd4b20368c4fe82594152f101
Bui Thi Kieu Chinh <80748999+kieuchinhxinh@users.noreply.github.com>
2023-03-17 03:11:00 +0700

    update qam document download

---
+++ Font-end/web1640/src/app/qam/document-download/document-download.component.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component( {
+
+  templateUrl: './document-download.component.html',
+  styleUrls: [ './document-download.component.css' ]
+} )
+
+
+export class DocumentDownloadComponent implements OnInit
+{
+  ngOnInit (): void
+  {
+
+  }
+
+}
+
