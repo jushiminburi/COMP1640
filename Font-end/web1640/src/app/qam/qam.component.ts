bf449d31b9130fa765c71c0f273db452e56a790c
Bui Thi Kieu Chinh <80748999+kieuchinhxinh@users.noreply.github.com>
2023-03-17 03:09:20 +0700

    update qam

--- Font-end/web1640/src/app/qam/qam.component.ts
+++ Font-end/web1640/src/app/qam/qam.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import PerfectScrollbar from 'perfect-scrollbar';
 
 @Component({
   selector: 'app-qam',
   templateUrl: './qam.component.html',
   styleUrls: ['./qam.component.css']
 })
-export class QamComponent {
+export class QamComponent implements  AfterViewInit, OnInit {
+  ngOnInit (): void
+  {
 
-  
+  }
+  constructor (  ) { }
+
+
+  ngAfterViewInit (): void
+  {
+    const socialListScrollbar = new PerfectScrollbar( '.dashboard-social-list' );
+    const topCountriesScrollbar = new PerfectScrollbar( '.dashboard-top-countries' );
+  }
+
+
+
 
 }
